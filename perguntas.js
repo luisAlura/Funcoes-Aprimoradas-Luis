d90396c7f623164c31882b43f92a4cd1d8baecff
luisAlura <107197542+luisAlura@users.noreply.github.com>
2024-10-25 10:46:43 -0300

    25

---
+++ perguntas.js
@@ -0,0 +1,97 @@
+export const perguntas = [
+    {
+        enunciado: "Você se encontra em uma floresta misteriosa e vê dois caminhos diante de você. Qual você escolhe?",
+        alternativas: [
+            {
+                texto: "O caminho iluminado por uma luz suave.",
+                afirmacao: [
+                    "Você encontrou uma floresta misteriosa e optou pelo caminho claro e seguro.",
+                    "Um caminho sem perigos com toda segurança"
+                ]    
+            },
+            {
+                texto: "O caminho envolto em névoa e sombras.",
+                afirmacao: [
+                    "Você encontrou uma floresta misteriosa e optou seguir pelo caminho misterioso e sombrio",
+                    "Um caminho muito perigoso sem saber oque podera encontrar nele"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Ao longo do caminho, você ouve um som estranho vindo dos arbustos.",
+        alternativas: [
+            {
+                texto: "Investiga o som, preparado para qualquer coisa.",
+                afirmacao: [
+                    "Ao ouvir um som estranho, enfrenta sem qualquer medo e seguro de sí.",
+                    "Afirmação 2"
+                ]
+            },
+            {
+                texto: "Ignora o som e segue em frente rapidamente.",
+                afirmacao: [
+                    "Ao ouvir um som estranho, o ignora e segue seu caminho rapidamente.",
+                    "Afirmação 2"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você encontra um viajante no caminho, ele parece cansado e perdido. O que você faz?",
+        alternativas: [
+            {
+                texto:"Oferece ajuda e compartilha seus mantimentos.",
+                afirmacao: [
+                    "Encontra um viajante, então oferece ajuda e compartilha seus mantimentos.",
+                    "Afirmação 2"
+                ]
+            },
+            {
+                texto: "Continua sua jornada sem parar, focado em seu destino.",
+                afirmacao: [
+                    "Encontra um viajante, porém, decide seguir sem parar, focado em seu destino.",
+                    "Afirmação 2"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "A noite cai e você precisa encontrar um lugar para descansar. Onde você decide parar?",
+        alternativas: [
+            {
+                texto: "Em uma caverna próxima, que parece segura.",
+                afirmacao: [
+                    "Em uma tomada de decisão, em encontrar um lugar para passar a noite, você vai acampar em uma caverna que parece segura.",
+                    "Afirmação 2"
+                ]
+            },
+            {
+                texto: "Em uma clareira ao ar livre, sob as estrelas.",
+                afirmacao: [
+                    "Em uma tomada de decisão, em encontrar um lugar para passar a noite, decide ir para uma clareira ao ar livre, com vista para as estrelas.",
+                    "Afirmação 2"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "No final de sua jornada, você encontra um portal mágico. Qual será sua escolha?",
+        alternativas: [
+            {
+                texto: "Entra no portal, curioso sobre o que há do outro lado.",
+                afirmacao: [
+                    "Já no final da jornada, encontra um portal, que decide entrar pela sua curiosidade.",
+                    "Afirmação 2"
+                ]
+            },
+            {
+                texto: "Evita o portal, preferindo explorar o mundo ao redor.",
+                afirmacao: [
+                    "Já no final da jornada, encontra um portal, mas decide evitar qualquer risco, pois quer explorar o mundo.",
+                    "Afirmação 2"
+                ]
+            }
+        ]
+    },
+];
